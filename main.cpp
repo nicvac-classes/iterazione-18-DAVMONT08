9b1902b5d160c904675f1737f8065d37979ce449
DAVMONT08 <davide.montella@marconibari.edu.it>
2024-10-31 09:37:16 +0000

    CONSEGNA

--- main.cpp
+++ main.cpp
@@ -2,10 +2,36 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+               float i,s,l,vm;
+               i=1;
+               s=0;
+               while(i<=7)    
+               {
+                  cout<<"inserire il livello dell'acqua"<<endl;
+                  cin>> l;
+                  s=s+l;
+                  if(l>0)
+                  {
+                    if(l>10)
+                    {
+                        cout<<"livello alto"<<endl;
+                        cout<<"nel giorno" << i << "acqua supera il livello di 10 cm"<<endl;
+                    }
+                    else
+                    {
+                        cout<<"livello alto"<<endl;
+                    }
+                  }
+                  else
+                  {
+                     cout<<"livello normale"<<endl;
+                    
+                  }
+                  i=i+1;
+                  vm=s/7;
+                  cout<<"la media del livello acqua è"<< vm <<endl;
+
+               
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
